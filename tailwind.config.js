cbec3a1bd3faf3952e24eb65f5e705390aee0540
WevertonLeo <wevertonardo17@gmail.com>
2025-09-25 23:19:04 -0300

    portifolio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3B82F6',
+        secondary: '#60A5FA',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
